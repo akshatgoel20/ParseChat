76f4e6ab78d899ecbd6810bc0ca2e7224c1a89d0
Akshat Goel <akshatishu@gmail.com>
2018-09-27 11:13:49 -0700

    first commit

---
+++ parseChat/Views/ChatCell.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    @IBOutlet weak var chatMessageLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
